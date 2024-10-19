c9b56588b6b1489e4d1ba3cc46011dd4416a2c04
Lucifurry <thelucifurry@gmail.com>
2024-10-19 12:51:00 +0700

    Other: some configs files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,8 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu({
+  rules: {
+    'no-restricted-syntax': 'off',
+    'antfu/top-level-function': 'off',
+  },
+})
