505369de2b80ae1cd84a26d5f340821bf02b381b
Lucifurry <thelucifurry@gmail.com>
2024-11-10 20:01:51 +0700

    Ref `stdlib/mixin`: remove array support and replace function by factory class

--- packages/stdlib/src/utils/mixin.test.ts
+++ packages/stdlib/src/utils/mixin.test.ts
@@ -1,4 +1,4 @@
-import { Mixin, mixin } from './mixin'
+import { Mixin } from './Mixin.create'
 
 const createFooConstructor = () => {
   return class Foo {
@@ -12,7 +12,21 @@ const createBarConstructor = () => {
     barMethod() { return 'bar return' }
   }
 }
-const createEmpConstructor = () => {
+const createDynastyConstructor = () => {
+  class Grandfather {
+    constructor(public grandfatherArg: string = 'grandfather default') { }
+    grandfatherMethod() { return 'grandfather return' }
+  }
+  class Father extends Grandfather {
+    constructor(public fatherArg: string = 'father default') { super() }
+    fatherMethod() { return 'father return' }
+  }
+  return class Son extends Father {
+    constructor(public sonArg: string = 'son default') { super() }
+    sonMethod() { return 'son return' }
+  }
+}
+const createNullaryConstructor = () => {
   return class Bar {
     constructor() { }
     empMethod() { return 'emp return' }
@@ -21,37 +35,39 @@ const createEmpConstructor = () => {
 
 let Foo = createFooConstructor()
 let Bar = createBarConstructor()
-let Emp = createEmpConstructor()
+let Nullary = createNullaryConstructor()
+let Dynasty = createDynastyConstructor()
 beforeEach(() => {
   Foo = createFooConstructor()
   Bar = createBarConstructor()
-  Emp = createEmpConstructor()
+  Nullary = createNullaryConstructor()
+  Dynasty = createDynastyConstructor()
 })
+
+// it('basic array', () => {
+//   const Mixed = Mixin.create([
+//     Foo,
+//     Bar,
+//     Nullary,
+//   ] as const)
+//   const mixed = new Mixed([
+//     [687],
+//     ['bar'],
+//     [],
+//   ])
+
+//   expect(mixed.fooArg).toBe(687)
+//   expect(mixed.barArg).toBe('bar')
+//   expect(mixed.fooMethod()).toBe('foo return')
+//   expect(mixed.barMethod()).toBe('bar return')
+//   expect(mixed.empMethod()).toBe('emp return')
+// })
 
 it('basic', () => {
-  const Mixed = mixin([
-    Foo,
-    Bar,
-    Emp,
-  ] as const)
-  const mixed = new Mixed([
-    [687],
-    ['bar'],
-    [],
-  ])
-
-  expect(mixed.fooArg).toBe(687)
-  expect(mixed.barArg).toBe('bar')
-  expect(mixed.fooMethod()).toBe('foo return')
-  expect(mixed.barMethod()).toBe('bar return')
-  expect(mixed.empMethod()).toBe('emp return')
-})
-
-it('basic record', () => {
-  const Mixed = mixin({
+  const Mixed = Mixin.create({
     foo: Foo,
     bar: Bar,
-    emp: Emp,
+    emp: Nullary,
   })
   const mixed = new Mixed({
     foo: [687],
@@ -66,30 +82,44 @@ it('basic record', () => {
 })
 
 it('must extend base Mixin abstract class', () => {
-  const Mixed = mixin([Emp])
-  const mixed = new Mixed([])
+  const Mixed = Mixin.create({ n: Nullary })
+  const mixed = new Mixed({})
 
   expect(mixed).toBeInstanceOf(Mixin)
 })
 
 it('mixin check consist constructor', () => {
-  const Mixed = mixin({
+  const Mixed = Mixin.create({
     foo: Foo,
     bar: Bar,
-    emp: Emp,
+    emp: Nullary,
   })
   const mixed = new Mixed({ foo: [], bar: [] })
 
   expect(Mixin.consistOf(mixed, Foo)).toBe(true)
   expect(Mixin.consistOf(mixed, Bar)).toBe(true)
-  expect(Mixin.consistOf(mixed, Emp)).toBe(true)
+  expect(Mixin.consistOf(mixed, Nullary)).toBe(true)
   expect(Mixin.consistOf(mixed, Set)).toBe(false)
   expect(Mixin.consistOf(mixed, Map)).toBe(false)
   expect(Mixin.consistOf(mixed, Number)).toBe(false)
 })
 
+it('keep members for long extend chain', () => {
+  const Mixed = Mixin.create({
+    dyn: Dynasty,
+  })
+  const mixed = new Mixed()
+
+  expect(mixed.sonArg).toBe('son default')
+  expect(mixed.sonMethod()).toBe('son return')
+  expect(mixed.fatherArg).toBe('father default')
+  expect(mixed.fatherMethod()).toBe('father return')
+  expect(mixed.grandfatherArg).toBe('grandfather default')
+  expect(mixed.grandfatherMethod()).toBe('grandfather return')
+})
+
 // it('instanceof', () => {
-//   const Mixed = mixin({ foo: Foo, bar: Bar })
+//   const Mixed = Mixin.create({ foo: Foo, bar: Bar })
 //   const mixed = new Mixed({
 //     foo: [123],
 //     bar: ['bar'],
--- packages/stdlib/src/utils/mixin.ts
+++ packages/stdlib/src/utils/mixin.ts
@@ -1,10 +1,10 @@
-import type { Constructor, FnGuard, OmitByValue, UnionToIntersection, ValuesType } from '@webshrine/stdtyp'
+import type { Constructor, OmitByValue, UnionToIntersection, ValuesType } from '@webshrine/stdtyp'
 
 /**
  * TODO
  * @see https://www.typescriptlang.org/docs/handbook/mixins.html
  */
-export function applyMembers(derivedConstructor: Constructor, constructors: MixinArray) {
+export function applyMembers(derivedConstructor: Constructor, constructors: ConstituentsArray) {
   constructors.forEach((baseCtor) => {
     Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
       Object.defineProperty(
@@ -18,79 +18,37 @@ export function applyMembers(derivedConstructor: Constructor, constructors: Mixi
 
 const constituents = Symbol('constituents')
 
-export abstract class Mixin {
-  static consistOf(instance: Mixin, constructor: Constructor) {
-    return instance[constituents].includes(constructor)
-  }
+type ConstituentsRecord = Record<string, Constructor>
+type ConstituentsArray = ReadonlyArray<Constructor>
 
-  protected [constituents]: MixinArray
-
-  constructor(extendedCtors: MixinArray) {
-    this[constituents] = extendedCtors
-  }
-
-  [Symbol.dispose](): void {
-    this[constituents].forEach((c) => {
-      c.prototype[Symbol.dispose]?.call(this)
-    })
-  }
-}
-
-type MixinRecord = Record<string, Constructor>
-type MixinArray = ReadonlyArray<Constructor>
-
-type MixedConstructorParamsFromRecord<Ctors extends MixinRecord> = OmitByValue<{
+type MixedConstructorParamsFromRecord<Ctors extends ConstituentsRecord> = OmitByValue<{
   [P in keyof Ctors]: ConstructorParameters<Ctors[P]> extends []
   ? undefined
   : ConstructorParameters<Ctors[P]>
 }, undefined>
 
-type MixedConstructorParamsFromArray<Ctors extends Readonly<MixinArray>> = {
-  [K in keyof Ctors]: ConstructorParameters<Ctors[K]>
-}
+type MixedConstructorParamsFromRecordOptionalize<Ctors extends ConstituentsRecord> =
+  MixedConstructorParamsFromRecord<Ctors> extends { [K in keyof Ctors]: Ctors[K] | undefined }
+  ? MixedConstructorParamsFromRecord<Ctors> | undefined
+  : MixedConstructorParamsFromRecord<Ctors>
 
-const isParametersAMixinArray = ((v: any) => Array.isArray(v)) as FnGuard<MixinArray>
-
-export function mixin<
-  Constructors extends MixinRecord | MixinArray,
-  MixedConstructorParams = Constructors extends MixinArray
-  ? MixedConstructorParamsFromArray<Constructors>
-  : Constructors extends MixinRecord ? MixedConstructorParamsFromRecord<Constructors> : never,
->(constructors: Constructors): Constructors extends MixinArray
-  ? Constructor<
-    UnionToIntersection<Mixin | InstanceType<Constructors[number]>>,
-    [MixedConstructorParamsFromArray<Constructors>]
-  >
-  : Constructors extends MixinRecord ? Constructor<
-    UnionToIntersection<Mixin | InstanceType<ValuesType<Constructors>>>,
-    [MixedConstructorParams]
-  > : never {
-  if (isParametersAMixinArray(constructors)) {
-    const ctorsList = constructors as MixinArray
-
-    class NewMixin extends Mixin implements Disposable {
-      constructor(parameters: MixedConstructorParamsFromArray<typeof ctorsList>) {
-        super(ctorsList)
-        for (let i = 0; i < ctorsList.length; i++) {
-          const Ctor = ctorsList[i]
-          const instance = new Ctor(...(parameters[i] || []))
-          Object.assign(this, instance)
-        }
-        // TODO: think about binding context to methods
-        // TODO: warn about overrides for dev mode
-      }
-    }
-
-    applyMembers(NewMixin, ctorsList)
-
-    // @ts-expect-error Complex type flow
-    return NewMixin
+export abstract class Mixin {
+  static consistOf(instance: MixinContainer, constructor: Constructor) {
+    return instance[constituents].includes(constructor)
   }
-  else {
-    const ctorsRecord = constructors as MixinRecord
+
+  static create<
+    Constructors extends ConstituentsRecord,
+    MixedConstructorParams = MixedConstructorParamsFromRecord<Constructors>,
+  >(constructors: Constructors,
+  ): Constructor<
+    UnionToIntersection<MixinContainer | InstanceType<ValuesType<Constructors>>>,
+    [MixedConstructorParams]
+  > {
+    const ctorsRecord = constructors as ConstituentsRecord
     const ctorsList = Object.values(ctorsRecord) as Constructor[]
 
-    class NewMixin extends Mixin implements Disposable {
+    class NewMixin extends MixinContainer implements Disposable {
       constructor(parameters: MixedConstructorParamsFromRecord<typeof ctorsRecord>) {
         super(ctorsList)
         for (const [key, Ctor] of Object.entries(ctorsRecord)) {
@@ -109,3 +67,85 @@ export function mixin<
     return NewMixin
   }
 }
+
+/**
+ * @private
+ */
+export abstract class MixinContainer {
+  protected [constituents]: ConstituentsArray
+
+  constructor(extendedCtors: ConstituentsArray) {
+    this[constituents] = extendedCtors
+  }
+
+  [Symbol.dispose](): void {
+    this[constituents].forEach((c) => {
+      c.prototype[Symbol.dispose]?.call(this)
+    })
+  }
+}
+
+// type MixedConstructorParamsFromArray<Ctors extends Readonly<MixinArray>> = {
+//   [K in keyof Ctors]: ConstructorParameters<Ctors[K]>
+// }
+
+// const isMixinArray = (v => Array.isArray(v)) as FnGuard<MixinArray>
+
+// export function mixin<
+//   Constructors extends MixinRecord | MixinArray,
+//   MixedConstructorParams = Constructors extends MixinArray
+//   ? MixedConstructorParamsFromArray<Constructors>
+//   : Constructors extends MixinRecord ? MixedConstructorParamsFromRecord<Constructors> : never,
+// >(constructors: Constructors): Constructors extends MixinArray
+//   ? Constructor<
+//     UnionToIntersection<Mixin | InstanceType<Constructors[number]>>,
+//     [MixedConstructorParamsFromArray<Constructors>]
+//   >
+//   : Constructors extends MixinRecord ? Constructor<
+//     UnionToIntersection<Mixin | InstanceType<ValuesType<Constructors>>>,
+//     [MixedConstructorParams]
+//   > : never {
+//   if (isMixinArray(constructors)) {
+//     const ctorsList = constructors as MixinArray
+
+//     class NewMixin extends Mixin implements Disposable {
+//       constructor(parameters: MixedConstructorParamsFromArray<typeof ctorsList>) {
+//         super(ctorsList)
+//         for (let i = 0; i < ctorsList.length; i++) {
+//           const Ctor = ctorsList[i]
+//           const instance = new Ctor(...(parameters[i] || []))
+//           Object.assign(this, instance)
+//         }
+//         // TODO: think about binding context to methods
+//         // TODO: warn about overrides for dev mode
+//       }
+//     }
+
+//     applyMembers(NewMixin, ctorsList)
+
+//     // @ts-expect-error Complex type flow
+//     return NewMixin
+//   }
+//   else {
+//     const ctorsRecord = constructors as MixinRecord
+//     const ctorsList = Object.values(ctorsRecord) as Constructor[]
+
+//     class NewMixin extends Mixin implements Disposable {
+//       constructor(parameters: MixedConstructorParamsFromRecord<typeof ctorsRecord>) {
+//         super(ctorsList)
+//         for (const [key, Ctor] of Object.entries(ctorsRecord)) {
+//           const args = parameters?.[key] || []
+//           const instance = new Ctor(...args)
+//           Object.assign(this, instance)
+//         }
+//         // TODO: think about binding context to methods
+//         // TODO: warn about overrides for dev mode
+//       }
+//     }
+
+//     applyMembers(NewMixin, ctorsList)
+
+//     // @ts-expect-error Complex type flow
+//     return NewMixin
+//   }
+// }
