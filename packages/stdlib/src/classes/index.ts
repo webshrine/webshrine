abf31be1d22c664c3ee88c67831ab6a221ce54e9
Lucifurry <thelucifurry@gmail.com>
2024-11-10 20:21:01 +0700

    Fix `stdlib/mixin`: fix imports

--- packages/stdlib/src/classes/index.ts
+++ packages/stdlib/src/classes/index.ts
@@ -1,3 +1,4 @@
 export * from './Autoincrement'
 export * from './Disposer'
 export * from './EventEmitter'
+export * from './Mixin'
--- packages/stdlib/src/utils/index.ts
+++ packages/stdlib/src/utils/index.ts
@@ -1,3 +1,2 @@
 export * from './clone'
 export * from './common'
-export * from './mixin'
