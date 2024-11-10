81331cc51a1d88a66cdfc6323f34f2afbe0f13b9
Lucifurry <thelucifurry@gmail.com>
2024-11-10 15:54:30 +0700

    Ref `stdlib`: use wrapper utils JSDoc comments from lodash

--- packages/stdlib/src/wrappers/index.ts
+++ packages/stdlib/src/wrappers/index.ts
@@ -1,14 +1,7 @@
-import type { Fn, FnPredicate, FnWrapper } from '@webshrine/stdtyp'
-import {
-  debounce as lodashDebounce,
-  memoize as lodashMemoize,
-  negate as lodashNegate,
-  once as lodashOnce,
-  throttle as lodashThrottle,
+export {
+  debounce,
+  memoize,
+  negate,
+  once,
+  throttle,
 } from 'lodash'
-
-export const debounce = lodashDebounce satisfies FnWrapper
-export const memoize = lodashMemoize satisfies FnWrapper
-export const negate: FnWrapper<FnPredicate> = lodashNegate
-export const once: FnWrapper<Fn, undefined> = lodashOnce
-export const throttle = lodashThrottle satisfies FnWrapper
