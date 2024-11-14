f1b138821f0d68c7864265b95d5323056ee8597a
Lucifurry <thelucifurry@gmail.com>
2024-11-14 12:28:07 +0700

    Docs: fix matchers functions JSDocs

--- packages/stdlib/src/matchers/index.ts
+++ packages/stdlib/src/matchers/index.ts
@@ -1,5 +1,41 @@
-export {
-  eq as areSame,
-  isEqual as areEqual,
+import type { FnMatch } from '@webshrine/stdtyp'
+
+import {
+  eq,
+  isEqual,
 } from 'lodash'
 
+/**
+ * Performs a [`SameValueZero`](http://ecma-international.org/ecma-262/6.0/#sec-samevaluezero)
+ * comparison between two values to determine if they are equivalent.
+ *
+ * @example
+ * var object = { 'user': 'fred' };
+ * var other = { 'user': 'fred' };
+ *
+ * areSame(object, object); // => true
+ * areSame(object, other); // => false
+ * areSame('a', 'a'); // => true
+ * areSame('a', Object('a')); // => false
+ * areSame(NaN, NaN); // => true
+ */
+export const areSame = eq satisfies FnMatch
+
+/**
+ * Performs a deep comparison between two values to determine if they are
+ * equivalent.
+ *
+ * **Note:** This method supports comparing arrays, array buffers, booleans,
+ * date objects, error objects, maps, numbers, `Object` objects, regexes,
+ * sets, strings, symbols, and typed arrays. `Object` objects are compared
+ * by their own, not inherited, enumerable properties. Functions and DOM
+ * nodes are **not** supported.
+ *
+ * @example
+ * var object = { 'user': 'fred' };
+ * var other = { 'user': 'fred' };
+ *
+ * areEqual(object, other); // => true
+ * object === other; // => false
+ */
+export const areEqual = isEqual satisfies FnMatch
