0f2b21afa24be1ec9158078a210172d2133f8d94
Lucifurry <thelucifurry@gmail.com>
2024-11-14 11:33:30 +0700

    Docs: fix page width jumping on scrollbar appear

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import DefaultTheme from 'vitepress/theme'
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import './style.css'
+import './scrollbar.css'
 
 export default {
   extends: DefaultTheme,
