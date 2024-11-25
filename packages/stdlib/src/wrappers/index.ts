6f47281d9c97a77e2ef7afec3d4aa61e11b682ed
Lucifurry <thelucifurry@gmail.com>
2024-11-25 10:45:30 +0700

    Perf `stdlib`: replace `lodash/once` by custom implementation to decrease bundle size and improve performance (70KB > 67KB)

--- packages/stdlib/src/wrappers/index.ts
+++ packages/stdlib/src/wrappers/index.ts
@@ -1,7 +1,24 @@
+import type { Fn, FnWrapper } from '@webshrine/stdtyp'
+
 export {
   debounce,
   memoize,
   negate,
-  once,
   throttle,
 } from 'lodash-es'
+
+/**
+ * Creates a function that is restricted to invoking func once. Repeat calls to the function return the value
+ * of the first call. The func is invoked with the this binding and arguments of the created function.
+ */
+export const once = (<T extends Fn>(fn: T): T => {
+  let wasCalled = false
+  let result: any
+  return ((...args: any[]) => {
+    if (!wasCalled) {
+      wasCalled = true
+      result = fn(...args)
+    }
+    return result
+  }) as T
+}) satisfies FnWrapper
