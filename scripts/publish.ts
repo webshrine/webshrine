5747f85b07ba1065de6d58a0f61acde057a422e6
Lucifurry <thelucifurry@gmail.com>
2024-11-26 10:49:31 +0700

    Ops: setup CI publish

---
+++ scripts/publish.ts
@@ -0,0 +1,18 @@
+import { execSync } from 'node:child_process'
+import path from 'node:path'
+import { consola } from 'consola'
+import { name as scope, version } from '../package.json'
+
+const PKGS = ['stdlib', 'stdtyp']
+
+execSync('npm run build', { stdio: 'inherit' })
+
+let command = 'npm publish --access public'
+
+if (version.includes('beta'))
+  command += ' --tag beta'
+
+for (const name of PKGS) {
+  execSync(command, { stdio: 'inherit', cwd: path.join('packages', name, 'dist') })
+  consola.success(`Published @${scope}/${name}`)
+}
