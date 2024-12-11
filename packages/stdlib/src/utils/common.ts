70b343c5661b45447d60b8746a9d20161929fdb4
Lucifurry <thelucifurry@gmail.com>
2024-12-11 12:36:44 +0700

    Feat `stdlib`: add `through` common util

--- packages/stdlib/src/utils/common.ts
+++ packages/stdlib/src/utils/common.ts
@@ -4,8 +4,24 @@ export * from 'cope'
 
 /**
  * A no-operation function that returns undefined regardless of the arguments it receives.
+ *
+ * Uses as mock in rare cases to satisfy types and decrease the amount of code.
  */
 export const noop = () => { }
+
+/**
+ * A no-operation function that returns the argument it received.
+ *
+ * Uses as mock in rare cases to satisfy types and decrease the amount of code.
+ * @example
+ * ```ts
+ * const order: FnWrapper = isDesc ? inverse : through
+ * const sorter = order(compareNumber)
+ *
+ * [1,3,2].sort(sorter)
+ * ```
+ */
+export const through = <T>(arg: T) => arg
 
 /**
  *
