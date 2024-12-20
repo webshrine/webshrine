a62a8bb8856baf78b58d37c0c5ebb4102d8b8448
Lucifurry <thelucifurry@gmail.com>
2024-12-20 10:31:49 +0700

    Fix `vue`: types of flat instance for `defineComposable`

--- packages/vue/src/definers/defineComposable.ts
+++ packages/vue/src/definers/defineComposable.ts
@@ -1,8 +1,10 @@
-import type { AnyArray, AnyArrayOptional, Fn } from '@webshrine/stdlib'
+import type { UnwrapRefs } from '@/utils'
+import type { AnyArrayOptional, AnyObject, Fn } from '@webshrine/stdlib'
 import { unwrapRefs } from '@/utils'
 import { once, pipe, through } from '@webshrine/stdlib'
 
-type ComposableInstance = Record<PropertyKey, any>
+type ComposableInstance = AnyObject
+type ComposableSetup = Fn<AnyArrayOptional, ComposableInstance>
 
 interface DefineComposableOptions {
 
@@ -12,18 +14,18 @@ interface DefineComposableOptions {
   /** Ensures that the composable is only created once and returns the same instance on subsequent calls */
   singleton?: boolean
 
-  setup: Fn<AnyArrayOptional, ComposableInstance>
+  setup: ComposableSetup
 }
 
-const flatter = (r: DefineComposableOptions['setup']) =>
-  (...args: AnyArray) => unwrapRefs(r(...args))
+const flatter = <S extends DefineComposableOptions['setup']>(setup: S) =>
+  (...args: Parameters<S>) => unwrapRefs(setup(...args)) as UnwrapRefs<ReturnType<S>>
 
 /**
  * Creates composable function with declarative control of behaviour.
  */
-export function defineComposable<
-  Options extends DefineComposableOptions,
->(options: Options): Options['setup'] {
+export function defineComposable<O extends DefineComposableOptions>(
+  options: O,
+): O['flatten'] extends true ? ReturnType<typeof flatter<O['setup']>> : O['setup'] {
   return pipe(
     options.flatten ? flatter : through,
     options.singleton ? once : through,
--- packages/vue/src/utils/unwrapRefs.ts
+++ packages/vue/src/utils/unwrapRefs.ts
@@ -1,17 +1,16 @@
-import type { MaybeRef, Ref } from 'vue'
+import type { AnyObject } from '@webshrine/stdlib'
+import type { Ref } from 'vue'
 import { forValues } from '@webshrine/stdlib'
 import { isRef } from 'vue'
 
-type IObjectWithRefs = Record<PropertyKey, MaybeRef<any>>
-
-type UnwrapRefs<T> = {
+export type UnwrapRefs<T> = {
   [K in keyof T]: T[K] extends Ref ? T[K]['value'] : T[K]
 } & {}
 
 /**
  *
  */
-export function unwrapRefs<T extends IObjectWithRefs>(target: T): UnwrapRefs<T> {
+export function unwrapRefs<T extends AnyObject>(target: T): UnwrapRefs<T> {
   const result: Record<PropertyKey, any> = {}
   forValues(target, (value, key) => {
     if (isRef(value)) {
