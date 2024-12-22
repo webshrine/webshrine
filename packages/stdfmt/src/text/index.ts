c35cd750ca0aa3bb35898afef12bf5765b3e7382
Lucifurry <thelucifurry@gmail.com>
2024-12-22 21:55:27 +0700

    Feat `stdfmt`: add casing formatters

--- packages/stdfmt/src/text/index.ts
+++ packages/stdfmt/src/text/index.ts
@@ -1,4 +1,28 @@
-/**
- *
- */
-export const toCamelCase = (text: string) => text
+import type { FnTransform } from '@webshrine/stdtyp'
+import {
+  camelCase,
+  capitalCase,
+  constantCase,
+  dotCase,
+  kebabCase,
+  noCase,
+  pascalCase,
+  pascalSnakeCase,
+  pathCase,
+  sentenceCase,
+  snakeCase,
+  trainCase,
+} from 'change-case'
+
+export const toCamelCase = camelCase satisfies FnTransform
+export const toCapitalCase = capitalCase satisfies FnTransform
+export const toConstantCase = constantCase satisfies FnTransform
+export const toDotCase = dotCase satisfies FnTransform
+export const toKebabCase = kebabCase satisfies FnTransform
+export const toNoCase = noCase satisfies FnTransform
+export const toPascalCase = pascalCase satisfies FnTransform
+export const toPascalSnakeCase = pascalSnakeCase satisfies FnTransform
+export const toPathCase = pathCase satisfies FnTransform
+export const toSentenceCase = sentenceCase satisfies FnTransform
+export const toSnakeCase = snakeCase satisfies FnTransform
+export const toTrainCase = trainCase satisfies FnTransform
