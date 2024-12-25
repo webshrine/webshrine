cfd73ff49aa40fccd87df9aab5cac7a297114ad1
Lucifurry <thelucifurry@gmail.com>
2024-12-25 11:53:59 +0700

    Build: add `generateAutoCategories` script

---
+++ scripts/generateAutoCategories.ts
@@ -0,0 +1,50 @@
+import type { JSDocableNode } from 'ts-morph'
+import * as path from 'node:path'
+import { Project, SyntaxKind } from 'ts-morph'
+
+const project = new Project()
+project.addSourceFilesAtPaths([
+  './packages/stdlib/src/**/*.ts',
+  '!./packages/stdlib/src/**/*.test.ts',
+] as const)
+
+const kindsToCategorize = [
+  SyntaxKind.FunctionDeclaration,
+  SyntaxKind.ClassDeclaration,
+  SyntaxKind.InterfaceDeclaration,
+  SyntaxKind.TypeAliasDeclaration,
+  SyntaxKind.EnumDeclaration,
+  SyntaxKind.VariableStatement,
+  SyntaxKind.VariableDeclaration,
+]
+
+function isJSDocableNode(node: any): node is JSDocableNode {
+  return kindsToCategorize.includes(node.getKind())
+}
+
+function setJSDocTag(node: JSDocableNode, tagName: string, text: string) {
+  const jsDocs = node.getJsDocs()
+  if (jsDocs.length > 0) {
+    jsDocs.forEach((doc) => {
+      doc.getTags().find(tag => tag.getTagName() === tagName)?.remove()
+    })
+
+    jsDocs[0].addTag({ tagName, text })
+  }
+  else {
+    node.addJsDoc({ tags: [{ tagName, text }] })
+  }
+}
+
+project.getSourceFiles().forEach((sourceFile) => {
+  const category = path.basename(path.dirname(sourceFile.getFilePath()))
+
+  sourceFile.forEachChild((node) => {
+    if (isJSDocableNode(node))
+      setJSDocTag(node, 'category', category)
+  })
+
+  sourceFile.saveSync()
+})
+
+console.log('JSDoc @category tags updated.')
