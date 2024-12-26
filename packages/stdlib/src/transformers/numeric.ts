3efa552b77f77eba622fda94bb27a57a5390abd4
Lucifurry <thelucifurry@gmail.com>
2024-12-26 10:55:04 +0700

    Fix `stdlib`: improve `clamp` typings and return correct type on out of range

--- packages/stdlib/src/transformers/numeric.ts
+++ packages/stdlib/src/transformers/numeric.ts
@@ -1,13 +1,31 @@
 import type { Numeric } from '@webshrine/stdtyp'
 
-/**
- * Clamps a numeric between a minimum and maximum value.
- * @category Transformers
- */
-export const clamp = (numeric: Numeric, min: Numeric, max: Numeric): Numeric => (
+const clampBase = <T extends Numeric>(numeric: T, min: T, max: T): T => (
   numeric > max
     ? max
     : numeric < min
       ? min
       : numeric
 )
+
+/**
+ * Clamps a number between a minimum and maximum value.
+ * @category Transformers
+ */
+export const clampNumber = clampBase<number>
+
+/**
+ * Clamps a bigint between a minimum and maximum value.
+ * @category Transformers
+ */
+export const clampBigInt = clampBase<bigint>
+
+/**
+ * Clamps a numeric between a minimum and maximum value.
+ * @category Transformers
+ */
+export const clamp = <T extends Numeric>(numeric: T, min: Numeric, max: Numeric): T => (
+  typeof numeric === 'number'
+    ? clampNumber(numeric, Number(min), Number(max))
+    : clampBigInt(numeric, BigInt(min), BigInt(max))
+) as T
