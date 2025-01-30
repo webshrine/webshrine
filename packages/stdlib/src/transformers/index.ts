53511c2ce1b9b7c4af6bb8109513fb0219f13b91
Lucifurry <thelucifurry@gmail.com>
2025-01-30 13:00:42 +0700

    Typo: some code formatting

--- packages/stdlib/src/transformers/index.ts
+++ packages/stdlib/src/transformers/index.ts
@@ -1,6 +1,6 @@
 export * from './common'
+export * from './dedupe'
 export * from './mergers'
 export * from './numerable'
 export * from './omitters'
 export * from './pickers'
-export * from './dedupe'
